2c3f6f9e8adde2faab8ff27af645dcf1ac29cb93
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-18 12:23:03 +0200

    Add TwoFactorVerifications table to handle two-factor authentication codes
    - Created `TwoFactorVerifications` table to store unique two-factor authentication (2FA) codes and their expiration dates.
    - Added foreign key relationship with `Users` to link 2FA codes to user accounts.
    - Enforced unique constraints on `user_id` and `code` to ensure each user has a distinct 2FA code.
    - Set expiration date field to manage the validity of 2FA codes.
    
    Implements two-factor authentication support in the schema.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -102,3 +102,16 @@ CREATE TABLE ResetPasswordVerifications
     CONSTRAINT UQ_ResetPasswordVerifications_User_Id UNIQUE (user_id),
     CONSTRAINT UQ_ResetPasswordVerifications_Url UNIQUE (url)
 );
+
+DROP TABLE IF EXISTS TwoFactorVerifications;
+
+CREATE TABLE TwoFactorVerifications
+(
+    id              BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id         BIGINT UNSIGNED NOT NULL,
+    code            VARCHAR(10)     NOT NULL,
+    expiration_date DATETIME        NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT UQ_TwoFactorVerifications_User_Id UNIQUE (user_id),
+    CONSTRAINT UQ_TwoFactorVerifications_Code UNIQUE (code)
+);
