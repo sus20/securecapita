9b45f18fc656180e57269a416ec74d1b788c5955
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-18 11:55:41 +0200

    Add Events and UserEvents tables with event tracking functionality
    - Created `Events` table to log various user actions such as login attempts, profile updates, and account changes, with validation on event types.
    
    - Added `UserEvents` table to track specific user events, including device information and IP address.
    
    - Added foreign key constraints between `Users`, `Events`, and `UserEvents` tables for relationship management.
    
    - Updated `created_date` to `created_at` in the `Users` table for consistency.
    
    Enhances schema to support event logging and auditing.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,20 +10,20 @@ DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users
 (
-    id           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name   VARCHAR(50)     NOT NULL,
-    last_name    VARCHAR(50)     NOT NULL,
-    email        VARCHAR(100)    NOT NULL,
-    password     VARCHAR(255) DEFAULT NULL,
-    address      VARCHAR(255) DEFAULT NULL,
-    phone        VARCHAR(30)  DEFAULT NULL,
-    title        VARCHAR(50)  DEFAULT NULL,
-    bio          VARCHAR(255) DEFAULT NULL,
-    enabled      BOOLEAN      DEFAULT FALSE,
-    non_locked   BOOLEAN      DEFAULT TRUE,
-    using_mfa    BOOLEAN      DEFAULT FALSE,
-    created_date DATETIME     DEFAULT CURRENT_TIMESTAMP,
-    image_url    VARCHAR(255) DEFAULT 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+    id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50)     NOT NULL,
+    last_name  VARCHAR(50)     NOT NULL,
+    email      VARCHAR(100)    NOT NULL,
+    password   VARCHAR(255) DEFAULT NULL,
+    address    VARCHAR(255) DEFAULT NULL,
+    phone      VARCHAR(30)  DEFAULT NULL,
+    title      VARCHAR(50)  DEFAULT NULL,
+    bio        VARCHAR(255) DEFAULT NULL,
+    enabled    BOOLEAN      DEFAULT FALSE,
+    non_locked BOOLEAN      DEFAULT TRUE,
+    using_mfa  BOOLEAN      DEFAULT FALSE,
+    created_at DATETIME     DEFAULT CURRENT_TIMESTAMP,
+    image_url  VARCHAR(255) DEFAULT 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
     CONSTRAINT UQ_Users_Email UNIQUE (email)
 );
 
@@ -48,4 +48,31 @@ CREATE TABLE UserRoles
     FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (role_id) REFERENCES Roles (id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT UQ_UserRoles_User_Id UNIQUE (user_id)
+);
+
+DROP TABLE IF EXISTS Events;
+
+CREATE TABLE Events
+(
+    id          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    type        VARCHAR(50)     NOT NULL CHECK ( type IN
+                                                 ('LOGIN_ATTEMPT', 'LOGIN_ATTEMPT_FAILURE', 'LOGIN_ATTEMPT_SUCCESS',
+                                                  'PROFILE_UPDATE', 'PROFILE_PICTURE_UPDATE', 'ROLE_UPDATE',
+                                                  'ACCOUNT_SETTING_UPDATE', 'PASSWORD_UPDATE', 'MFA_UPDATE') ),
+    description VARCHAR(255)    NOT NULL,
+    CONSTRAINT UQ_Events_Type UNIQUE (type)
+);
+
+DROP TABLE IF EXISTS UserEvents;
+
+CREATE TABLE UserEvents
+(
+    id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id    BIGINT UNSIGNED NOT NULL,
+    event_id   BIGINT UNSIGNED NOT NULL,
+    device     VARCHAR(100) DEFAULT NULL,
+    ip_address VARCHAR(100) DEFAULT NULL,
+    created_at DATETIME     DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES Events (id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
