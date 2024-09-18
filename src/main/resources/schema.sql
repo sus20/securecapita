83e3522966f36447ac2d1526672ec1f1a0dfabe8
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-18 12:11:35 +0200

    Add ResetPasswordVerifications table to manage password reset URLs
    - Created `ResetPasswordVerifications` table to store unique password reset URLs and their expiration dates.
    - Added foreign key relationship with `Users` to link password reset requests to user accounts.
    - Enforced unique constraints on `user_id` and `url` to ensure each user has a unique reset URL.
    - Set expiration date field for managing validity of reset requests.
    
    Introduces functionality for handling secure password reset processes.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -83,9 +83,22 @@ CREATE TABLE AccountVerifications
 (
     id      BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT UNSIGNED NOT NULL,
-    url     VARCHAR(255) DEFAULT NULL,
+    url     VARCHAR(255) NOT NULL,
     -- date DATETIME NOT NULL ,
     FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT UQ_AccountVerifications_User_Id UNIQUE (user_id),
     CONSTRAINT UQ_AccountVerifications_Url UNIQUE (url)
+);
+
+DROP TABLE IF EXISTS ResetPasswordVerifications;
+
+CREATE TABLE ResetPasswordVerifications
+(
+    id              BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id         BIGINT UNSIGNED NOT NULL,
+    url             VARCHAR(255)    NOT NULL,
+    expiration_date DATETIME        NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT UQ_ResetPasswordVerifications_User_Id UNIQUE (user_id),
+    CONSTRAINT UQ_ResetPasswordVerifications_Url UNIQUE (url)
 );
