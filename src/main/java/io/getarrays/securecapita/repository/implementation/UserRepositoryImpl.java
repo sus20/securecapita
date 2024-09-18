1d91c4fc96960283e1be314ebf99da2d211a171b
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-18 15:36:14 +0200

    Implement user creation logic with email uniqueness check, role assignment, and verification URL generation. Added bcrypt password encryption and exception handling.

--- src/main/java/io/getarrays/securecapita/repository/implementation/UserRepositoryImpl.java
+++ src/main/java/io/getarrays/securecapita/repository/implementation/UserRepositoryImpl.java
@@ -1,31 +1,69 @@
 package io.getarrays.securecapita.repository.implementation;
 
+import io.getarrays.securecapita.domain.Role;
 import io.getarrays.securecapita.domain.User;
+import io.getarrays.securecapita.exception.ApiException;
+import io.getarrays.securecapita.repository.RoleRepository;
 import io.getarrays.securecapita.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.UUID;
+
+import static io.getarrays.securecapita.enumeration.RoleType.ROLE_USER;
+import static io.getarrays.securecapita.enumeration.VerificationType.ACCOUNT;
+import static io.getarrays.securecapita.query.UserQuery.*;
+import static java.util.Map.of;
+import static java.util.Objects.requireNonNull;
 
 @Repository
 @RequiredArgsConstructor
 @Slf4j
 public class UserRepositoryImpl implements UserRepository<User> {
     private final NamedParameterJdbcTemplate jdbcTemplate ;
+    private final RoleRepository<Role> roleRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
-    public User create(User data) {
-        // check the email is unique
+    public User create(User user) {
+        // Check the email is unique
+        if(getEmailCount(user.getEmail().trim().toLowerCase()) > 0) throw new ApiException("Email already in use. Please use a different email and try again.");
         // Save new user
-        // Add role to the user
-        // Send verification URL
-        // Send URL in verification table
-        // Send email to user with verification URL
-        // Return the newly created user
-        // If any errors, throw exception with proper message
-        return null;
+        try{
+            KeyHolder holder = new GeneratedKeyHolder();
+            SqlParameterSource parameterSource = getSqlParameterSource(user);
+            jdbcTemplate.update(INSERT_USER_QUERY, parameterSource, holder);
+            user.setId(requireNonNull(holder.getKey()).longValue());
+            // Add role to the user
+            roleRepository.addRoleToUser(user.getId(), ROLE_USER.name());
+            // Send verification URL
+            String verificationUrl = getVerificationUrl(UUID.randomUUID().toString(), ACCOUNT.getType());
+            // Send URL in verification table
+            jdbcTemplate.update(INSERT_ACCOUNT_VERIFICATION_URL_QUERY, of("userId", user.getId(), "url",verificationUrl));
+            // Send email to user with verification URL
+            // emailService.sendVerificationUrl(user.getFirstName(), user.getEmail(), verificationUrl, ACCOUNT);
+            user.setEnabled(false);
+            user.setNotLocked(true);
+            // Return the newly created user
+            return user;
+            // If any errors, throw exception with proper message
+        }catch (EmptyResultDataAccessException exception){
+            throw new ApiException("No role found by name: " + ROLE_USER.name());
+
+        } catch (Exception exception){
+            throw new ApiException("An error occurred. Please try again.");
+        }
     }
 
     @Override
@@ -46,5 +84,24 @@ public class UserRepositoryImpl implements UserRepository<User> {
     @Override
     public Boolean delete(Long id) {
         return null;
+    }
+
+    private Integer getEmailCount(String email) {
+        return jdbcTemplate.queryForObject(COUNT_USER_EMAIL_QUERY, of("email", email), Integer.class );
+    }
+
+    private SqlParameterSource getSqlParameterSource(User user) {
+        return new MapSqlParameterSource()
+                .addValue("firstName", user.getFirstName())
+                .addValue("lastName", user.getLastName())
+                .addValue("email", user.getEmail())
+                .addValue("password", bCryptPasswordEncoder.encode(user.getPassword()));
+    }
+
+    private String getVerificationUrl(String key, String type){
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/user/verify" + type + "/" + key)
+                .toUriString();
     }
 }
