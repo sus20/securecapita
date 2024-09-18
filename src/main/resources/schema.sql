3c4bce088598b4d9c2e6278717331fe61be70f01
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-18 11:35:10 +0200

    Add Roles and UserRoles tables with foreign key constraints
    - Created the `Roles` table with a unique role name and permission field.
    - Created the `UserRoles` table to establish a many-to-many relationship between users and roles.
    - Added foreign key constraints for `user_id` and `role_id` with cascading and restriction rules.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,3 @@
-
 CREATE SCHEMA IF NOT EXISTS TS securecapita;
 
 SET NAMES 'UTF8MB4';
@@ -11,20 +10,42 @@ DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users
 (
-    id                  BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    first_name          VARCHAR(50) NOT NULL ,
-    last_name           VARCHAR(50) NOT NULL ,
-    email               VARCHAR(100) NOT NULL ,
-    password            VARCHAR(255) DEFAULT NULL,
-    address             VARCHAR(255) DEFAULT NULL,
-    phone               VARCHAR(30) DEFAULT NULL,
-    title               VARCHAR(50) DEFAULT NULL,
-    bio                 VARCHAR(255) DEFAULT NULL,
-    enabled             BOOLEAN DEFAULT FALSE,
-    non_locked          BOOLEAN DEFAULT TRUE,
-    using_mfa           BOOLEAN DEFAULT FALSE,
-    created_date        DATETIME DEFAULT CURRENT_TIMESTAMP,
-    image_url           VARCHAR(255) DEFAULT 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+    id           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name   VARCHAR(50)     NOT NULL,
+    last_name    VARCHAR(50)     NOT NULL,
+    email        VARCHAR(100)    NOT NULL,
+    password     VARCHAR(255) DEFAULT NULL,
+    address      VARCHAR(255) DEFAULT NULL,
+    phone        VARCHAR(30)  DEFAULT NULL,
+    title        VARCHAR(50)  DEFAULT NULL,
+    bio          VARCHAR(255) DEFAULT NULL,
+    enabled      BOOLEAN      DEFAULT FALSE,
+    non_locked   BOOLEAN      DEFAULT TRUE,
+    using_mfa    BOOLEAN      DEFAULT FALSE,
+    created_date DATETIME     DEFAULT CURRENT_TIMESTAMP,
+    image_url    VARCHAR(255) DEFAULT 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
     CONSTRAINT UQ_Users_Email UNIQUE (email)
+);
 
+DROP TABLE IF EXISTS Roles;
+
+CREATE TABLE Roles
+(
+    id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(50)     NOT NULL,
+    permission VARCHAR(255)    NOT NULL,
+    CONSTRAINT UQ_Roles_Name UNIQUE (name)
+
+);
+
+DROP TABLE IF EXISTS UserRoles;
+
+CREATE TABLE UserRoles
+(
+    id      BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    role_id BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES Roles (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT UQ_UserRoles_User_Id UNIQUE (user_id)
 );
