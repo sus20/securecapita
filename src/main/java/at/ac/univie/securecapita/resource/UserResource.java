3b4bc50d7ea88d92017872eee6c733fb0faca5a0
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-08-01 10:44:58 +0200

    Add UserResource with user registration endpoint.

---
+++ src/main/java/at/ac/univie/securecapita/resource/UserResource.java
@@ -0,0 +1,46 @@
+package at.ac.univie.securecapita.resource;
+
+import at.ac.univie.securecapita.domain.HttpResponse;
+import at.ac.univie.securecapita.domain.User;
+import at.ac.univie.securecapita.dto.UserDTO;
+import at.ac.univie.securecapita.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+import static java.time.LocalTime.now;
+import static java.util.Map.of;
+import static org.springframework.http.HttpStatus.CREATED;
+
+
+@RestController
+@RequestMapping(path = "/user")
+@RequiredArgsConstructor
+public class UserResource {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<HttpResponse> saveUser(@RequestBody @Valid User user) {
+        UserDTO userDTO = userService.createUser(user);
+        return ResponseEntity.created(getUri() ).body(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .data(of("user", userDTO))
+                        .message("User created")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .build()
+        );
+    }
+
+    private URI getUri() {
+        return URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/get/<userId>").toUriString());
+    }
+}
