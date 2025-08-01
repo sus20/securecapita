066ec2011be87eda6be6dfd827c670e7ef4cffb2
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-08-01 09:34:01 +0200

    Add RoleRowMapper to map SQL result sets to Role objects.

---
+++ src/main/java/at/ac/univie/securecapita/rowmapper/RoleRowMapper.java
@@ -0,0 +1,19 @@
+package at.ac.univie.securecapita.rowmapper;
+
+import at.ac.univie.securecapita.domain.Role;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RoleRowMapper implements RowMapper<Role> {
+
+    @Override
+    public Role mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        return Role.builder()
+                .id(resultSet.getLong("id"))
+                .name(resultSet.getString("name"))
+                .permission(resultSet.getString("permission"))
+                .build();
+    }
+}
