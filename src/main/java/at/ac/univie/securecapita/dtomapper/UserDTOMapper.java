6f08ca244f3ab42f5d885c311b3c49d4fd3f0947
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-08-01 10:06:06 +0200

    Add UserDTOMapper for converting between User and UserDTO.

---
+++ src/main/java/at/ac/univie/securecapita/dtomapper/UserDTOMapper.java
@@ -0,0 +1,21 @@
+package at.ac.univie.securecapita.dtomapper;
+
+import at.ac.univie.securecapita.domain.User;
+import at.ac.univie.securecapita.dto.UserDTO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDTOMapper {
+    public static UserDTO fromUser(User user) {
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(user, userDTO);
+        return userDTO;
+    }
+
+    public static User toUser(UserDTO userDTO) {
+        User user = new User();
+        BeanUtils.copyProperties(userDTO, user);
+        return user;
+    }
+}
