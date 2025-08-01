de73a6d8bc5929ccfbc12d3777f6b00aaf9cab9c
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-08-01 10:01:21 +0200

    Add schema.sql with initial roles data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -126,3 +126,8 @@ CREATE TABLE TwoFactorVerifications
     CONSTRAINT UQ_TwoFactorVerifications_Code UNIQUE (code)
 );
 
+INSERT INTO Roles (name, permission) VALUES
+                                         ('ROLE_USER', 'READ:USER,READ:CUSTOMER'),
+                                         ('ROLE_MANAGER', 'READ:USER,READ:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER'),
+                                         ('ROLE_ADMIN', 'READ:USER,READ:CUSTOMER,CREATE:USER,CREATE:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER'),
+                                         ('ROLE_SYSADMIN', 'READ:USER,READ:CUSTOMER,CREATE:USER,CREATE:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER,DELETE:USER,DELETE:CUSTOMER');
