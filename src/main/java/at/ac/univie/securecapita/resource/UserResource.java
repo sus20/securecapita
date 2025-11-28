87634fed16cea87592dbcbda49a0de28ab791912
Sushma Sapkota <a01363028@unet.univie.ac.at>
2025-11-28 10:32:52 +0100

    Add login endpoint and integrate AuthenticationManager into UserResource

--- src/main/java/at/ac/univie/securecapita/resource/UserResource.java
+++ src/main/java/at/ac/univie/securecapita/resource/UserResource.java
@@ -7,6 +7,7 @@ import at.ac.univie.securecapita.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,17 +20,27 @@ import static java.time.LocalTime.now;
 import static java.util.Map.of;
 import static org.springframework.http.HttpStatus.CREATED;
 
+import org.springframework.security.authentication.AuthenticationManager;
+
 
 @RestController
 @RequestMapping(path = "/user")
 @RequiredArgsConstructor
 public class UserResource {
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<HttpResponse> login(String email, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+
+        return null;
+    }
 
     @PostMapping("/register")
     public ResponseEntity<HttpResponse> saveUser(@RequestBody @Valid User user) {
         UserDTO userDTO = userService.createUser(user);
-        return ResponseEntity.created(getUri() ).body(
+        return ResponseEntity.created(getUri()).body(
                 HttpResponse.builder()
                         .timeStamp(now().toString())
                         .data(of("user", userDTO))
